d156f532d945d3af41cba055843961cd4e9a006d
Kevin Keith <kevinrikeith@outlook.com>
2023-07-24 11:31:31 -0400

    Added Extra Page

---
+++ MusicPlayers/ViewModels/FileExplorerViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MusicPlayers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayers.ViewModels
+{
+    public class FileExplorerViewModel : BaseViewModel
+    {
+        public FileExplorerViewModel() 
+        {
+            
+        }
+    }
+}
--- MusicPlayers/ViewModels/MusicViewModel.cs
+++ MusicPlayers/ViewModels/MusicViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using MusicPlayers.Misc;
 using MusicPlayers.Models;
+using MusicPlayers.Views;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Media;
@@ -16,7 +18,9 @@ namespace MusicPlayers.ViewModels
     {
         public string PlayPause { get; set; }
         public ICommand PlayCommand { get; set; }
+        public ICommand FileCommand { get; set; }
         private bool _isPlaying {get; set;}
+        ObservableCollection<String> _playlists { get; set;}
         public string CurrentSong { get; set; }
         private SoundPlayer player;
         string path;
@@ -26,10 +30,17 @@ namespace MusicPlayers.ViewModels
             path = Directory.GetCurrentDirectory();
             path = Directory.GetParent(path).FullName;
             path = Directory.GetParent(path).FullName;
+            string[] fileNames = Directory.GetFiles($"{path}//Music");
             PlayPause = $"{path}\\Images\\PlayButton2.png";
             PlayCommand = new DelegateCommand(playButtonCommand);
             _isPlaying = false;
             CurrentSong = "Hello World";
+            FileCommand = new DelegateCommand(FileButton);
+        }
+        private void FileButton()
+        {
+            FileExplorer files = new FileExplorer();
+            files.Show();
         }
         private void playButtonCommand()
         {
